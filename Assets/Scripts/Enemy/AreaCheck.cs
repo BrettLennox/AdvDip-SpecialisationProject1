e242b51314bd3cd1532a7fb213f4ccd5ce772070
BrettLennox <brettheaton@hotmail.com>
2023-03-01 12:28:34 +1100

    player detection started to work

--- Assets/Scripts/Enemy/AreaCheck.cs
+++ Assets/Scripts/Enemy/AreaCheck.cs
@@ -7,10 +7,28 @@ using UnityEngine;
 public class AreaCheck : MonoBehaviour
 {
     public bool canSeePlayer;
+    public List<GameObject> sortedList;
 
     // Update is called once per frame
     void Update()
     {
+        CastSphereFromPosition();
+    }
 
+    public void CastSphereFromPosition()
+    {
+        RaycastHit hit;
+
+        List<Collider> sphereList = Physics.OverlapSphere(transform.position, 10f).ToList();
+
+        for(int i = 0; i < sphereList.Count; i++)
+        {
+            if(sphereList[i].gameObject.GetComponent<IDamageable>() != null)
+            {
+                if(sortedList.Contains(sphereList[i].gameObject)) { return; }
+                sortedList.Add(sphereList[i].gameObject);
+                Debug.Log($"ADDING {sphereList[i].name.ToUpper()} TO NEW LIST");
+            }
+        }
     }
 }
